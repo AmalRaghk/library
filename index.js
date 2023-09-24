bd61e5af6271c044502a19750ce0dd8ad7cbe4df
Amal Ragh K <amalraghk123@gmail.com>
2023-09-24 16:22:02 +0530

    added to the const library

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-cards=document.querySelector('.cards');
-addbtn=document.querySelector('#addbtn');
-bookDial=document.querySelector('#bookDial')
+const cards=document.querySelector('.cards');
+const addbtn=document.querySelector('#addbtn');
+const bookDial=document.querySelector('#bookDial');
+const title=document.querySelector('#title');
+const Author=document.querySelector('#Author');
+const Pagenos=document.querySelector('#Pagenos');
+const Read=document.querySelector('select[name="Read"]')
 const myLibrary = [];
+var k;
 
 function Book() {
+  const book={
+    "Title":'...',
+    "Author":'...',
+    "Pagenos":'...',
+    "Read":'...'
+  }
+  return book
 
 }
 
-function addBookToLibrary() {
-  
+function addBookToLibrary(book,Title,Author,Pagenos,Read) {
+  book["Title"]=Title;
+  book["Author"]=Author;
+  book["Pagenos"]=Pagenos;
+  book["Read"]=Read;
+  return book
 }
 bookDial.addEventListener("close", (e) => {
-  outputBox.value =
     bookDial.returnValue === "default"
       ? "No return value."
       : `ReturnValue: ${bookDial.returnValue}.`; // Have to check for "default" rather than empty string
@@ -20,4 +35,13 @@ addbtn.addEventListener("click", (event) => {
     event.preventDefault(); 
     bookDial.showModal();
 
+  });
+  confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    k=Book()
+    k=addBookToLibrary(k,title.value,Author.value,Pagenos.value,Read.value)
+    myLibrary.push(k)
+    console.log(myLibrary)
+    bookDial.close();
+    
   });
